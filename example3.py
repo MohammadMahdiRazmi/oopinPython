7f048a60a68c40d7584294d7886cb6cc5424b732
MahdiRazmi <109846619+MohammadMahdiRazmi@users.noreply.github.com>
2023-09-15 21:32:58 +0330

    oppinPython

---
+++ example3.py
@@ -0,0 +1,21 @@
+class gamer:
+    game = "callofduty"
+    mood = "multiplayer"
+    rank = "legend"
+    level = 300
+alex = gamer()
+class car :
+    def __init__(self,company,model,speed):
+        self.company = company
+        self.model = model
+        self.speed = speed
+print(alex.game)
+print(alex.mood)
+print(alex.rank)
+print(alex.level)
+car1 = car("ford","mostang",320)
+print(car1.company)
+print(car1.model)
+print(car1.speed)
+print()
+
